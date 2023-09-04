6a92e9ad391be7c04b9630354cf97dbfbd28c711
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-09-04 19:19:08 -0300

    Implementação de sistema utilizando vetor, conjuntos e comparadores

---
+++ DistinctStudentAccessCounter/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DistinctStudentAccessCounter.Entities
+{
+    internal class Student
+    {
+        public int Code { get; set; }
+
+        public Student(int code)
+        {
+            Code = code;
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student other = obj as Student;
+            return Code.Equals(other.Code);
+        }
+    }
+}
---
+++ DistinctStudentAccessCounter/Program.cs
@@ -0,0 +1,66 @@
+﻿using DistinctStudentAccessCounter.Entities;
+using System;
+using System.IO;
+
+namespace AccessCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<Student> set = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    int codeUser = int.Parse(Console.ReadLine());
+                    set.Add(new Student(codeUser));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    int codeUser = int.Parse(Console.ReadLine());
+                    set.Add(new Student(codeUser));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    int codeUser = int.Parse(Console.ReadLine());
+                    set.Add(new Student(codeUser));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total students: " + set.Count);
+
+        }
+    }
+}
